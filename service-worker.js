3143910afabe1b8bb31be6c7a1f45ac407e63cd3
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-07-06 19:09:23 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Bhttps://ohasesung1.github.io/dgsw.petition%7D/precache-manifest.5fed2d6a46afbfeffa1e9111d9b6202a.js"
+  "/dgsw.petition/precache-manifest.53c31321d979c89c1faef7a6dd0f65eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bhttps://ohasesung1.github.io/dgsw.petition%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dgsw.petition/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
